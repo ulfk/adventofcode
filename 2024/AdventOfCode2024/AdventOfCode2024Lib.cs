233c7c9df4db83a4c60c3fb28eb693fafc376edd
Ulf Kühnle <ulf.kuehnle@gmx.de>
2024-12-05 20:39:41 +0100

    day 3 finished, day 4 started

--- 2024/AdventOfCode2024/AdventOfCode2024Lib.cs
+++ 2024/AdventOfCode2024/AdventOfCode2024Lib.cs
@@ -1,5 +1,10 @@
-﻿namespace AdventOfCode2024;
+﻿using System.Text.RegularExpressions;
 
+namespace AdventOfCode2024;
+
+/// <summary>
+/// https://adventofcode.com/2024
+/// </summary>
 public class AdventOfCode2024Lib
 {
     private static string[] SplitLines(string lines)
@@ -134,7 +139,113 @@ public class AdventOfCode2024Lib
 
     public static long Day03_1(string input)
     {
-        throw new NotImplementedException();
+        var matches = Regex.Matches(input, @"mul\(\d{1,3},\d{1,3}\)");
+        var sum = 0;
+
+        foreach (var match in matches)
+        {
+            var numbers = Regex.Matches(match.ToString()!,@"\d{1,3}").Select(x => int.Parse(x.ToString())).ToArray();
+            sum += numbers[0] * numbers[1];
+        }
+
+        return sum;
+    }
+    
+    public static long Day03_2(string input)
+    {
+        var matches = Regex.Matches(input, @"mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\)");
+        var sum = 0;
+        var enabled = true;
+
+        foreach (var match in matches)
+        {
+            var cmd = match.ToString()!;
+            if (cmd == "don't()")
+            {
+                enabled = false;
+                continue;
+            }
+
+            if (cmd == "do()")
+            {
+                enabled = true;
+                continue;
+            }
+
+            if (enabled)
+            {
+                var numbers = Regex.Matches(match.ToString()!,@"\d{1,3}").Select(x => int.Parse(x.ToString())).ToArray();
+                sum += numbers[0] * numbers[1];
+            }
+        }
+
+        return sum;
+    }
+    
+    #endregion
+    
+    #region Day04
+
+    public static long Day04_1(string input)
+    {
+        var lines = SplitLines(input);
+        var width = lines[0].Length;
+        var height = lines.Length;
+        var sum = 0;
+        var existMap = new int[height,width];
+
+        for (var x = 0; x < width; x++)
+        {
+            for (var y = 0; y < height; y++)
+            {
+                var add = 0;
+                if (lines[y][x] == 'X')
+                {
+                    var horizontalRight = lines[y][SaveCalc(x + 1, width)] == 'M'
+                                       && lines[y][SaveCalc(x + 2, width)] == 'A'
+                                       && lines[y][SaveCalc(x + 3, width)] == 'S' ? 1 : 0;
+                    var horizontalLeft =  lines[y][SaveCalc(x - 1, width)] == 'M' 
+                                       && lines[y][SaveCalc(x - 2, width)] == 'A'
+                                       && lines[y][SaveCalc(x - 3, width)] == 'S' ? 1 : 0;
+                    var verticalDown =  lines[SaveCalc(y + 1, height)][x] == 'M'
+                                     && lines[SaveCalc(y + 2, height)][x] == 'A'
+                                     && lines[SaveCalc(y + 3, height)][x] == 'S' ? 1 : 0;
+                    var verticalUp =    lines[SaveCalc(y - 1, height)][x] == 'M'
+                                     && lines[SaveCalc(y - 2, height)][x] == 'A'
+                                     && lines[SaveCalc(y - 3, height)][x] == 'S' ? 1 : 0;
+                    var diagDownRight = lines[SaveCalc(y + 1, height)][SaveCalc(x + 1, width)] == 'M'
+                                     && lines[SaveCalc(y + 2, height)][SaveCalc(x + 2, width)] == 'A'
+                                     && lines[SaveCalc(y + 3, height)][SaveCalc(x + 3, width)] == 'S' ? 1 : 0;
+                    var diagDownLeft =  lines[SaveCalc(y + 1, height)][SaveCalc(x - 1, width)] == 'M'
+                                     && lines[SaveCalc(y + 2, height)][SaveCalc(x - 2, width)] == 'A'
+                                     && lines[SaveCalc(y + 3, height)][SaveCalc(x - 3, width)] == 'S' ? 1 : 0;
+                    var diagUpRight =   lines[SaveCalc(y - 1, height)][SaveCalc(x + 1, width)] == 'M'
+                                     && lines[SaveCalc(y - 2, height)][SaveCalc(x + 2, width)] == 'A'
+                                     && lines[SaveCalc(y - 3, height)][SaveCalc(x + 3, width)] == 'S' ? 1 : 0;
+                    var diagUpLeft =    lines[SaveCalc(y - 1, height)][SaveCalc(x - 1, width)] == 'M'
+                                     && lines[SaveCalc(y - 2, height)][SaveCalc(x - 2, width)] == 'A'
+                                     && lines[SaveCalc(y - 3, height)][SaveCalc(x - 3, width)] == 'S' ? 1 : 0;
+                    add = horizontalRight + horizontalLeft 
+                         + verticalDown + verticalUp 
+                         + diagDownRight + diagDownLeft 
+                         + diagUpRight + diagUpLeft;
+                }
+                
+                if (add > 0)
+                {
+                    sum += add;
+                }            
+            }
+        }
+
+        return sum;
+    }
+
+    public static int SaveCalc(int value, int max)
+    {
+        if (value < 0) return max + value;
+        if (value >= max) return value - max;
+        return value;
     }
     
     #endregion
